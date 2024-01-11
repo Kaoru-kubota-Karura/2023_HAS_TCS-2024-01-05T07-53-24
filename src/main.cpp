fa2116998813113a7f7654e8e49b0e6f569393d3
Kaoru-kubota-Karura <cosmo.kaoru1211@gmail.com>
2024-01-11 19:09:44 +0800

    0111 TCS+AUTO

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,25 @@ using namespace vex;
 int main() {
  // Initializing Robot Configuration. DO NOT REMOVE!
  vexcodeInit();
+ front_l.spin(forward);
+ front_r.spin(forward);
+ back_l.spin(reverse);
+ back_r.spin(reverse);
+ wait(4, seconds);
+ front_l.spin(forward);
+ back_l.spin(reverse);
+ front_r.stop(hold);
+ back_r.stop(hold);
+ wait(1.5,seconds);
+ front_l.spin(forward);
+ front_r.spin(forward);
+ back_l.spin(reverse);
+ back_r.spin(reverse);
+ wait(2,seconds);
+ front_l.stop(coast);
+ back_l.stop(coast);
+ front_r.stop(coast);
+ back_r.stop(coast);
  while(1){
    front_l.spin(forward,Controller1.Axis3.position(percent),percent);
    // front_leftモータの速度ををコントローラーのAxis3の位置(パーセント)によって動かす
